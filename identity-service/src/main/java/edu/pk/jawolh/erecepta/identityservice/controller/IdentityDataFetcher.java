aa36caa6a2357ee8926f8ebae1cb15e783c68825
Margiorno <m.zawiski03@gmail.com>
2025-10-30 00:09:09 +0100

    Identity service controller endpoints set

--- identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/controller/IdentityDataFetcher.java
+++ identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/controller/IdentityDataFetcher.java
@@ -1,13 +1,37 @@
 package edu.pk.jawolh.erecepta.identityservice.controller;
 
+import com.example.demo.codegen.types.*;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.InputArgument;
 import lombok.RequiredArgsConstructor;
 
 @DgsComponent
 @RequiredArgsConstructor
 public class IdentityDataFetcher {
 
+    @DgsMutation
+    public Message register(@InputArgument RegisterInput input){
+        return Message.newBuilder().message("test").build();
+    }
 
+    @DgsMutation
+    public Message verifyAccount(@InputArgument VerifyInput input){
+        return Message.newBuilder().message("test").build();
+    }
 
+    @DgsMutation
+    public AuthToken login(@InputArgument LoginInput input){
+        return AuthToken.newBuilder().token("test").build();
+    }
 
+    @DgsMutation
+    public Message requestPasswordReset(@InputArgument ResetPasswordRequestInput input){
+        return Message.newBuilder().message("test").build();
+    }
+
+    @DgsMutation
+    public Message resetPassword(@InputArgument ResetPasswordInput input){
+        return Message.newBuilder().message("test").build();
+    }
 }
