35e003ced484f0ee1ba05a8a38c7e02f77ad7809
Margiorno <m.zawiski03@gmail.com>
2025-10-29 23:26:12 +0100

    Dgs framework added

---
+++ identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/controller/IdentityDataFetcher.java
@@ -0,0 +1,13 @@
+package edu.pk.jawolh.erecepta.identityservice.controller;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import lombok.RequiredArgsConstructor;
+
+@DgsComponent
+@RequiredArgsConstructor
+public class IdentityDataFetcher {
+
+
+
+
+}
