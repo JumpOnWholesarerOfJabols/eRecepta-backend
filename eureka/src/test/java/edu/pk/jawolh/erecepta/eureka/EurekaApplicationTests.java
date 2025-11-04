86621079e70c72f3a64d08f162caa3a6d34b0031
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2025-11-04 15:04:11 +0100

    Added Eureka discovery service and a simple gateway

---
+++ eureka/src/test/java/edu/pk/jawolh/erecepta/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pk.jawolh.erecepta.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
