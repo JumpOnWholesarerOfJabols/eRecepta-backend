a951b2ca05cb3bb03032c556ccc533cc464631b3
Margiorno <m.zawiski03@gmail.com>
2025-11-11 20:50:45 +0100

    reset password request and send verification code events publish on rabbitMQ server

---
+++ common-dto/src/main/java/edu/pk/jawolh/erecepta/common/user/messages/SendVerificationCodeMessage.java
@@ -0,0 +1,13 @@
+package edu.pk.jawolh.erecepta.common.user.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendVerificationCodeMessage {
+    private String email;
+    private String code;
+}
--- identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/client/RabbitMQClient.java
+++ identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/client/RabbitMQClient.java
@@ -1,13 +1,36 @@
 package edu.pk.jawolh.erecepta.identityservice.client;
 
+import edu.pk.jawolh.erecepta.common.user.messages.SendResetPasswordCodeMessage;
+import edu.pk.jawolh.erecepta.common.user.messages.SendVerificationCodeMessage;
+import edu.pk.jawolh.erecepta.identityservice.config.RabbitMqProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RabbitMQClient {
 
-    public void sendVerificationCodeEvent(String email, String verificationCode) {}
+    private final RabbitTemplate rabbitTemplate;
+    private final RabbitMqProperties rabbitMqProperties;
 
-    public void sendResetPasswordCodeEvent(String email, String resetPasswordCode) {}
+    public void sendVerificationCodeEvent(String email, String verificationCode) {
+        SendVerificationCodeMessage event = new SendVerificationCodeMessage(email, verificationCode);
+
+        rabbitTemplate.convertAndSend(
+                rabbitMqProperties.getExchangeName(),
+                rabbitMqProperties.getSendVerificationCodeRoutingKey(),
+                event
+        );
+    }
+
+    public void sendResetPasswordCodeEvent(String email, String resetPasswordCode) {
+        SendResetPasswordCodeMessage event = new SendResetPasswordCodeMessage(email, resetPasswordCode);
+
+        rabbitTemplate.convertAndSend(
+                rabbitMqProperties.getExchangeName(),
+                rabbitMqProperties.getSendResetPasswordRoutingKey(),
+                event
+        );
+    }
 }
