211b381c03249abefd6986b268dfca5241df1561
Margiorno <m.zawiski03@gmail.com>
2025-11-11 22:09:27 +0100

    mails sending works

--- notification-service/src/main/java/edu/pk/jawolh/erecepta/notificationservice/consumer/RabbitMqConsumer.java
+++ notification-service/src/main/java/edu/pk/jawolh/erecepta/notificationservice/consumer/RabbitMqConsumer.java
@@ -3,6 +3,7 @@ package edu.pk.jawolh.erecepta.notificationservice.consumer;
 import edu.pk.jawolh.erecepta.common.user.messages.ResetPasswordCodeMessage;
 import edu.pk.jawolh.erecepta.common.user.messages.VerificationCodeMessage;
 import edu.pk.jawolh.erecepta.notificationservice.configuration.RabbitMqProperties;
+import edu.pk.jawolh.erecepta.notificationservice.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,16 +14,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RabbitMqConsumer {
 
+    private final EmailService emailService;
+
     @RabbitListener(queues = "#{rabbitMqProperties.getVerificationCodeEventTopic()}")
     public void processVerificationCodeMessage(VerificationCodeMessage message) {
         log.info("Received verification code message: {}", message);
 
+        emailService.sendVerificationCode(message.getEmail(), message.getCode());
     }
 
     @RabbitListener(queues = "#{rabbitMqProperties.getResetPasswordCodeEventTopic()}")
     public void processResetPasswordCodeMessage(ResetPasswordCodeMessage message) {
         log.info("Received reset password code message: {}", message);
 
-
+        emailService.sendRestPasswordCode(message.getEmail(), message.getCode());
     }
 }
