bc91512f72b860c85c11ca529716ca0d179b6705
Margiorno <m.zawiski03@gmail.com>
2025-12-06 17:48:22 +0100

    controller mutations from user

--- patient-record-service/src/main/java/edu/pk/jawolh/erecepta/patientrecordservice/controller/PatientRecordDataFetcher.java
+++ patient-record-service/src/main/java/edu/pk/jawolh/erecepta/patientrecordservice/controller/PatientRecordDataFetcher.java
@@ -1,7 +1,9 @@
 package edu.pk.jawolh.erecepta.patientrecordservice.controller;
 
 import com.example.demo.codegen.types.PatientInfo;
+import com.example.demo.codegen.types.UpdatePatientInfoInput;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import edu.pk.jawolh.erecepta.patientrecordservice.service.PatientRecordService;
 import lombok.RequiredArgsConstructor;
@@ -19,4 +21,66 @@ public class PatientRecordDataFetcher {
         return patientRecordService.getPatientInfo(userId);
     }
 
+
+    @DgsMutation
+    public PatientInfo updatePatientInfo(UUID userId, UpdatePatientInfoInput input) {
+
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+    @DgsMutation
+    public PatientInfo addAllergy(UUID userId, String allergy) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+    @DgsMutation
+    public PatientInfo removeAllergy(UUID userId, String allergy) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+
+    @DgsMutation
+    public PatientInfo addMedication(UUID userId, String medication) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+    @DgsMutation
+    public PatientInfo removeMedication(UUID userId, String medication) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+
+    @DgsMutation
+    public PatientInfo addChronicDisease(UUID userId, String medication) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+    @DgsMutation
+    public PatientInfo removeChronicDisease(UUID userId, String medication) {
+        System.out.println("Test");
+
+        return PatientInfo.newBuilder().build();
+    }
+
+
+
+
+
+
+
+
+//    addChronicDisease(userId: ID!, disease: String!): PatientInfo
+//    removeChronicDisease(userId: ID!, disease: String!): PatientInfo
 }
