569d3ccb1e7c05049ad6d6b75cc3636c1d14f2bc
Margiorno <m.zawiski03@gmail.com>
2025-12-11 21:48:25 +0100

    medical documents service graphql schema

---
+++ medical-documents-service/src/main/java/edu/pk/jawolh/erecepta/med_docs_service/MedicalDocumentsServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.pk.jawolh.erecepta.med_docs_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDocumentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDocumentsServiceApplication.class, args);
+	}
+
+}
