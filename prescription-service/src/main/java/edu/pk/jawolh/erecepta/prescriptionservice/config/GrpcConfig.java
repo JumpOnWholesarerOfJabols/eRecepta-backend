794e819bd952979fc314853a00529db9a42f927e
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2025-12-20 16:07:04 +0100

    - Added beans for VisitServiceGrpc and PatientRecordServiceGrpc stubs

--- prescription-service/src/main/java/edu/pk/jawolh/erecepta/prescriptionservice/config/GrpcConfig.java
+++ prescription-service/src/main/java/edu/pk/jawolh/erecepta/prescriptionservice/config/GrpcConfig.java
@@ -1,7 +1,9 @@
 package edu.pk.jawolh.erecepta.prescriptionservice.config;
 
 import edu.pk.jawolh.erecepta.common.medication.proto.MedicationServiceGrpc;
+import edu.pk.jawolh.erecepta.common.record.proto.PatientRecordServiceGrpc;
 import edu.pk.jawolh.erecepta.common.user.proto.UserServiceGrpc;
+import edu.pk.jawolh.erecepta.common.visit.proto.VisitServiceGrpc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
@@ -16,6 +18,16 @@ public class GrpcConfig {
     @Bean
     public MedicationServiceGrpc.MedicationServiceBlockingStub grpcMedicationServiceStub(GrpcChannelFactory channels) {
         return MedicationServiceGrpc.newBlockingStub(channels.createChannel("medication"));
+    }
+
+    @Bean
+    public VisitServiceGrpc.VisitServiceBlockingStub grpcVisitServiceStub(GrpcChannelFactory channels) {
+        return VisitServiceGrpc.newBlockingStub(channels.createChannel("visit"));
+    }
+
+    @Bean
+    public PatientRecordServiceGrpc.PatientRecordServiceBlockingStub grpcRecordServiceStub(GrpcChannelFactory channels) {
+        return PatientRecordServiceGrpc.newBlockingStub(channels.createChannel("record"));
     }
 
 }
