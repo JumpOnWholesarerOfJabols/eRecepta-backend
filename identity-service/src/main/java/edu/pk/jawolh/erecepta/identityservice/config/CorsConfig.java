aaeb0665a7e8e7e95fa40b8f536f2983403a655c
Margiorno <m.zawiski03@gmail.com>
2026-01-01 14:36:39 +0100

    cors config updated

--- identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/config/CorsConfig.java
+++ identity-service/src/main/java/edu/pk/jawolh/erecepta/identityservice/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/graphql/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
